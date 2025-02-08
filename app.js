f107813fce28f59d0b43bbb74ce2e49c6da6af5a
M-Aitisam <aitisamahmed24@gmail.com>
2025-02-08 19:07:09 +0500

    Sucessfully using brcypt hash the password

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const express = require('express');
-const app =  express();
-const path  = require('path');
-const cookieparser =require('cookie-parser');
+const app = express();
+const path = require('path');
+const cookieparser = require('cookie-parser');
+const usermodel = require("./models/user")
+const brcypt = require('bcrypt');
 
-
-app.set("view engine" , "ejs");
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname , 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieparser());
 
-app.get("/" ,  (req,res)=>{
-    res.send("Aitisam");
+app.get("/", (req, res) => {
+    res.render("index");
 })
 
+app.post('/create', (req, res) => {
+    let { username, email, password, age } = req.body;
+brcypt.genSalt(10,(err,salt)=>{
+
+    brcypt.hash(password, salt, async (err, hash)=>{
+        let createdUser =   await usermodel.create({
+            username,
+            email,
+            password:hash,
+            age
+        });
+        res.send(createdUser);
+    
+    })
+})
+})
 app.listen(3000);
