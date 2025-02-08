5eb541ff1920263776f14246e2c80fa6efa19c9f
M-Aitisam <aitisamahmed24@gmail.com>
2025-02-08 23:04:35 +0500

    Completed Over Authentication and Authorization using JWT Token

--- app.js
+++ app.js
@@ -1,34 +1,68 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cookieparser = require('cookie-parser');
-const usermodel = require("./models/user")
-const brcypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const userModel = require("./models/user");
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieparser());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.render("index");
-})
+});
 
-app.post('/create', (req, res) => {
+app.post('/create', async (req, res) => {
+
     let { username, email, password, age } = req.body;
-brcypt.genSalt(10,(err,salt)=>{
 
-    brcypt.hash(password, salt, async (err, hash)=>{
-        let createdUser =   await usermodel.create({
-            username,
-            email,
-            password:hash,
-            age
-        });
-        res.send(createdUser);
-    
-    })
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    let createdUser = await userModel.create({
+        username,
+        email,
+        password: hash,
+        age
+    });
+
+    let token = jwt.sign({ email }, "aitisam" );
+    res.cookie("token", token, { httpOnly: true });
+
+    res.send({ message: "User created successfully", user: createdUser, token });
+});
+//login
+app.get("/login", (req, res) => {
+    res.render("login");
 })
+app.post("/login", async (req, res) => {
+    let user =  await userModel.findOne({ email: req.body.email });
+
+    if (!user) {  res.send("Something Went Wrong 🤔"); }
+    else {
+        let match = await bcrypt.compare(req.body.password, user.password);
+        if (match) {
+            let token = jwt.sign({ email: user.email }, "aitisam");
+            res.cookie("token", token, { httpOnly: true });
+            return  res.send("Yes, You can log in.");
+        } else {
+          return  res.send("Something Went Wrong 🤔");
+        }
+        
+
+    }
+        
 })
-app.listen(3000);
+//logout
+app.get("/logout", (req, res) => {
+    res.cookie("token", "");
+    res.redirect("/");
+})
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
